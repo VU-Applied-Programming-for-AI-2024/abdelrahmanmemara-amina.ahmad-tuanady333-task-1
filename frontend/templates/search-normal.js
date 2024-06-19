bc5519c605b6a58f66955d8eb2a581032594c56e
Abdelrahmanmemara <119069265+Abdelrahmanmemara@users.noreply.github.com>
2024-06-19 23:54:19 +0200

    extracting the inputs from the search page

---
+++ frontend/templates/search-normal.js
@@ -0,0 +1,17 @@
+
+
+document.getElementById('searchButton').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const origin = document.getElementById('origin').values;
+    const end = document.getElementById('final-destination').values;
+    const min_price = document.getElementById('min-price').values;
+    const max_price = document.getElementById('max-price').values;
+    const dept_date = document.getElementById('departure-date').values;
+    const ret_date = document.getElementById('return-date').values;
+
+    const query_string = `fromEntityId=${end}&toEntityid=${origin}&minimumPrice=${min_price}&max_price=${max_price}&
+    depart=${dept_date}&return_date=${ret_date}`;
+
+    window.location.href(`search-result-normal-flight.html?${query_string}`)
+})
