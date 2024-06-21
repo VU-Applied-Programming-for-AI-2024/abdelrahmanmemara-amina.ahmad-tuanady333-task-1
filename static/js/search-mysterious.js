2858e5320f3c2d27c479c4a89d7fdc427b932465
Abdelrahmanmemara <119069265+Abdelrahmanmemara@users.noreply.github.com>
2024-06-21 16:12:33 +0200

    Trying to fetch the inputs

--- static/js/search-mysterious.js
+++ static/js/search-mysterious.js
@@ -1,16 +1,16 @@
 
 
-document.getElementById('searchButton').addEventListener('submit', function(event) {
+document.getElementById('searchButton').addEventListener('click', function(event) {
     event.preventDefault();
 
-    const origin = document.getElementById('origin').values;
-    const min_price = document.getElementById('min-price').values;
-    const max_price = document.getElementById('max-price').values;
-    const dept_date = document.getElementById('departure-date').values;
-    const ret_date = document.getElementById('return-date').values;
+    const origin = document.getElementById('origin').values
+    const min_price = document.getElementById('min-price').value;
+    const max_price = document.getElementById('max-price').value;
+    const dept_date = document.getElementById('departure-date').value;
+    const ret_date = document.getElementById('return-date').value;
 
     const query_string = `fromEntityId=${origin}&minimumPrice=${min_price}&max_price=${max_price}&
     depart=${dept_date}&return_date=${ret_date}`;
 
-    window.location.href(`search-result-normal-flight.html?${query_string}`)
+    window.location.href(`search-result-mysterious-flight?${query_string}`)
 })
