b49f4a3c2038b7ccf8f60c30767575bf2695265b
Tuana Durmayüksel <tuana.durmayuksel@gmail.com>
2024-06-22 17:25:34 +0200

    mysterious loading page

--- static/js/results-mysterious.js
+++ static/js/results-mysterious.js
@@ -31,48 +31,33 @@ const europeanCities = [
     "Riga"
 ];
 
-
-
 function getRandomCity(Cities) {
-
     var number = Math.floor(Math.random() * Cities.length);
-
     return Cities[number];
 }
 
 var randomCity = getRandomCity(europeanCities);
-
-// function filterPrice(result) {
-//     var flights = [];
-
-//     while (flights.length < 6 & )
-
-
-// }
 
 function parameters() {
     var params = new URLSearchParams(window.location.search);
     return {
         origin: params.get('fromEntityId'),
         min_price: params.get('minimumPrice'),
-        max_price:params.get('max_price'),
+        max_price: params.get('max_price'),
         dept_date: params.get('depart'),
         return_date: params.get('return_date')
     }
 }
 
-
 var param = parameters();
 
 async function weatherAPI(date) {
-
     const fixed_date = date.slice(6,11);
     const url1 = `http://api.openweathermap.org/geo/1.0/direct?q=${randomCity}&limit=1&appid=76dcae341cd27c351c448e898edca058`;
     const first_response = await fetch(url1);
     const latitude_longitude = await first_response.json();
     const lon = latitude_longitude[0]['lon'];
     const lat = latitude_longitude[0]['lat'];
-
 
     const url = `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&date=2023-${fixed_date}&appid=76dcae341cd27c351c448e898edca058`
 
@@ -85,41 +70,36 @@ async function weatherAPI(date) {
 async function transformCity(city) {
     const url = `https://sky-scanner3.p.rapidapi.com/flights/auto-complete?query=${city}`;
     const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '76e3d7e612msha9b0c87d96f7361p175e04jsn7f8908586ac3',
-		'x-rapidapi-host': 'sky-scanner3.p.rapidapi.com'
-	    }
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '76e3d7e612msha9b0c87d96f7361p175e04jsn7f8908586ac3',
+            'x-rapidapi-host': 'sky-scanner3.p.rapidapi.com'
+        }
     };
 
     try {
-        let response = await fetch(url,options);
-
+        let response = await fetch(url, options);
         let result = await response.json();
-
         const cityId = result['data'][0]['presentation']['skyId'];
-
         return cityId;
-
-    } catch(error) {
+    } catch (error) {
         console.error(error)
     }
-} 
+}
 
-
-async function generateCard(result,cityId,cityTo, number) {
+async function generateCard(result, cityId, cityTo, number) {
     const html = document.getElementById('main');
     const city_name = populateAirportList(result);
-    const price = flightPrice(result,number);
-    const dept_time = departureTime(result,number);
-    const arrival_time = arrivalTime(result,number);
+    const price = flightPrice(result, number);
+    const dept_time = departureTime(result, number);
+    const arrival_time = arrivalTime(result, number);
     const airport = firstAirport(result);
     const weather = await weatherAPI(param['dept_date']);
     const temp = weather['temperature']['min'];
     const wind = weather['wind']['max']['speed'];
     const humidity = weather['humidity']['afternoon'];
     const percipitation = weather['precipitation']['total'];
-    const url = skyscannerLink(cityId,cityTo,param['dept_date'],param['return_date']);
+    const url = skyscannerLink(cityId, cityTo, param['dept_date'], param['return_date']);
     const markup = `<div class="row">
             <div class="col-md-12">
                 <div class="flight-card">
@@ -138,20 +118,18 @@ async function generateCard(result,cityId,cityTo, number) {
                 </div>
         </div>`;
     
-        html.insertAdjacentHTML('beforeend',markup);
+    html.insertAdjacentHTML('beforeend', markup);
 }
 
-
-
-let cachedData = null; // Variable to store cached API response data
+let cachedData = null;
 
 async function fetchFlights() {
     try {
+        document.getElementById('loadingAlert').style.display = 'block'; // Show loading alert
+        
         const cityId = await transformCity(param['origin']);
-
         const cityTo = await transformCity(randomCity);
-        // Construct the URL using the cityId
-        console.log(cityTo);
+
         const url = `https://sky-scanner3.p.rapidapi.com/flights/search-roundtrip?fromEntityId=${cityId}&toEntityId=${cityTo}&departDate=${param['dept_date']}&returnDate=${param['return_date']}&stops=direct,1stop`;
         const options = {
             method: 'GET',
@@ -161,84 +139,67 @@ async function fetchFlights() {
             }
         };
 
-        // Fetch the flight data
         const response = await fetch(url, options);
         const result = await response.json();
         var resultsLength = result['data']['itineraries'].length;
-        console.log(resultsLength);
-        // Process the result
-        if (resultsLength > 0 & resultsLength < 11) {
-            console.log(resultsLength);
+
+        if (resultsLength > 0 && resultsLength < 11) {
             document.getElementById('count').insertAdjacentHTML('beforeend', `<p><strong>Number of Flights Found:</strong> ${resultsLength}</p>`);
             for (let i = 0; i < resultsLength; i++) {
-                generateCard(result,cityId,cityTo,i);
+                generateCard(result, cityId, cityTo, i);
             }
-        }
-
-        else if (resultsLength > 10) {
-            document.getElementById('count').insertAdjacentHTML('beforeend','<p><strong>Number of Flights Found:</strong> 10 </p>');
+        } else if (resultsLength > 10) {
+            document.getElementById('count').insertAdjacentHTML('beforeend', '<p><strong>Number of Flights Found:</strong> 10 </p>');
             for (let i = 0; i < 10; i++) {
-                generateCard(result,cityId,cityTo,i);
+                generateCard(result, cityId, cityTo, i);
             }
-        }
-        else if (resultsLength === 0) {
+        } else if (resultsLength === 0) {
             document.getElementById('count').insertAdjacentHTML('beforeend', '<p><strong>Number of Flights Found:</strong> 0 </p>');
-            document.getElementById('main').insertAdjacentHTML('beforeend',`<div class="row">
+            document.getElementById('main').insertAdjacentHTML('beforeend', `<div class="row">
             <div class="col-md-12">
                 <div class="flight-card">
                 <p> No results found </p>
                 </div>
-        </div>`);
+            </div>`);
         }
+
+        document.getElementById('loadingAlert').style.display = 'none'; // Hide loading alert after results are loaded
     } catch (error) {
         console.error('Error fetching data:', error);
     }
 }
 
-function flightPrice(data,number) {
-
+function flightPrice(data, number) {
     const price = data['data']['itineraries'][number]['price']['formatted'];
-
-    return price
+    return price;
 }
 
 function populateAirportList(data) {
     const airports = data['data']['filterStats']['airports'];
-
     const markup = ` ${airports[0]['city']} &rarr; <span class="gift">🎁</span>`;
-   
     return markup;
 }
 
 function firstAirport(data) {
-
     const airports = data['data']['filterStats']['airports'][0]['airports'][0]['name'];
-
     return airports;
 }
 
-function departureTime(data,number) {
-
+function departureTime(data, number) {
     const time = data['data']['itineraries'][number]['legs'][0]['departure'].slice(11,16);
-
     return time;
 }
 
-function arrivalTime(data,number) {
-
+function arrivalTime(data, number) {
     const time = data['data']['itineraries'][number]['legs'][0]['arrival'].slice(11,16);
-
     return time;
 }
 
-function skyscannerLink(cityId,cityTo, dept_date, arrival_date) {
-    const dept = dept_date.slice(2,4)+ dept_date.slice(5,7) + dept_date.slice(8,10);
+function skyscannerLink(cityId, cityTo, dept_date, arrival_date) {
+    const dept = dept_date.slice(2,4) + dept_date.slice(5,7) + dept_date.slice(8,10);
     const arrival = arrival_date.slice(2,4) + arrival_date.slice(5,7) + arrival_date.slice(8,10);
     const url = `https://www.skyscanner.nl/transport/vluchten/${cityId}/${cityTo}/${dept}/${arrival}/?adultsv2=1&cabinclass=economy&childrenv2=&ref=home&rtn=1&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false`
     return url;
 }
 
-
-
-// Call the async function
 fetchFlights();
